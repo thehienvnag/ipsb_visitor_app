16e371282909c4281c2c1d4aad66f5d3b2d7483b
caoquynhtrang <trangcaoquynh1207@gmail.com>
2021-06-22 14:22:20 +0700

    feature/TrangCQ/Commit code store details

--- lib/src/pages/store_details/views/store_details_page.dart
+++ lib/src/pages/store_details/views/store_details_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
+
 import 'package:indoor_positioning_visitor/src/models/product.dart';
 import 'package:indoor_positioning_visitor/src/pages/store_details/controllers/store_details_controller.dart';
 
@@ -69,66 +70,110 @@ class StoreDetailsPage extends GetView<StoreDetailsController> {
             Expanded(
               child: TabBarView(
                 children: [
-                  ListView.separated(
-                      itemBuilder: (context, index) {
-                        Product product = listProduct[index];
-                        return Row(
-                          children: [
-                            Image.network(
-                              product.imageUrl ?? '',
-                              height: 100,
-                            ),
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Row(
-                                  children: [
-                                    Container(
-                                      margin: EdgeInsets.only(
-                                          right: 42, bottom: 5, top: 12),
-                                      child: Text(product.name ?? '',
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.w700)),
-                                    ),
-                                    Container(
-                                      child: Text(
-                                        '${product.price} VNĐ',
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.w700),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                Container(
-                                  width: 250,
-                                  child: Text(
-                                    product.description ?? '',
-                                    style: TextStyle(fontSize: 13),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        );
-                      },
-                      separatorBuilder: (context, index) => Divider(
-                            indent: 10,
-                            color: Colors.black,
-                          ),
-                      itemCount: listProduct.length),
-                  Column(
-                    children: [
-                      Image.network(
-                        'https://www.highlandscoffee.com.vn/vnt_upload/product/03_2018/TRASENVANG.png',
-                        height: 100,
-                      ),
-                    ],
-                  ),
+                  _buildProducts(),
+                  _buildCoupons(),
                 ],
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildProducts() {
+    return ListView.separated(
+      itemBuilder: (context, index) {
+        Product product = listProduct[index];
+        return Row(
+          children: [
+            Image.network(
+              product.imageUrl ?? '',
+              height: 100,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(right: 42, bottom: 5),
+                      child: Text(product.name ?? '',
+                          style: TextStyle(fontWeight: FontWeight.w700)),
+                    ),
+                    Container(
+                      child: Text(
+                        '${product.price} VNĐ',
+                        style: TextStyle(fontWeight: FontWeight.w700),
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  width: 250,
+                  child: Text(
+                    product.description ?? '',
+                    style: TextStyle(fontSize: 13),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => Divider(
+        indent: 30,
+        endIndent: 30,
+        color: Colors.black,
+      ),
+      itemCount: listProduct.length,
+    );
+  }
+
+  Widget _buildCoupons() {
+    return ListView.separated(
+      itemCount: coupons.length,
+      itemBuilder: (context, index) {
+        final coupon = coupons[index];
+        return Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(
+              coupon.imageUrl ?? '',
+              height: 80,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 10, left: 10),
+                  width: 240,
+                  child: Text(coupon.name ?? '',
+                      style: TextStyle(fontWeight: FontWeight.w700)),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 10, left: 10),
+                  width: 240,
+                  child: Text(
+                    coupon.description ?? '',
+                    style: TextStyle(fontSize: 13),
+                  ),
+                ),
+                TextButton.icon(
+                  onPressed: () {},
+                  icon: Icon(Icons.info),
+                  label: Text('Xem chi tiết'),
+                ),
+              ],
+            ),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => Divider(
+        indent: 30,
+        endIndent: 30,
+        color: Colors.black,
       ),
     );
   }
@@ -172,3 +217,49 @@ List<Product> listProduct = [
         'https://www.highlandscoffee.com.vn/vnt_upload/product/03_2018/TRASENVANG.png',
   ),
 ];
+
+final coupons = [
+  Coupon(
+    id: 1,
+    name: 'THỬ PHINDI MỚI!',
+    description:
+        'Thêm 6.000/ly để đổi ly PhinDi tặng sang cỡ vừa hoặc 10.000/ly để đổi sang cỡ lớn',
+    imageUrl:
+        'https://www.highlandscoffee.com.vn/vnt_upload/product/03_2018/TRASENVANG.png',
+  ),
+  Coupon(
+    id: 2,
+    name: 'CHỌN FREEZE TRÀ XANH, NHẬN THẺ TẬN HƯỞNG!',
+    description:
+        'Thưởng thức sự kết hợp hoàn hảo giữa vị Trà Xanh Đậm Đà cùng những miếng Thạch Giòn Khó Cưỡng.',
+    imageUrl:
+        'https://www.highlandscoffee.com.vn/vnt_upload/product/03_2018/TRASENVANG.png',
+  ),
+  Coupon(
+    id: 3,
+    name: 'SỔ ƯU ĐÃI MỪNG 300 QUÁN',
+    description:
+        'Từ ngày 07/07/2019 đến ngày 21/07/2019 tại Highlands Coffee® toàn quốc.',
+    imageUrl:
+        'https://www.highlandscoffee.com.vn/vnt_upload/product/03_2018/TRASENVANG.png',
+  ),
+  Coupon(
+    id: 4,
+    name: 'GIẢM 30K ĐƠN TỪ 100K',
+    description:
+        'E-Voucher giảm giá 30k cho đơn hàng 100k khi thanh toán bằng Airpay QR hoặc BLE Scan & Pay',
+    imageUrl:
+        'https://www.highlandscoffee.com.vn/vnt_upload/product/03_2018/TRASENVANG.png',
+  ),
+];
+
+class Coupon {
+  final int? id;
+  final String? name, description, imageUrl;
+  Coupon({
+    this.id,
+    this.name,
+    this.description,
+    this.imageUrl,
+  });
+}
