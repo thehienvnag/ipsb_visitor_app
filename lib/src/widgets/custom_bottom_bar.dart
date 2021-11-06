a55c591fa0d60756e95aba59a1051a2ea6a784e4
thehienvnag <thehienvnag@gmail.com>
2021-11-06 13:00:41 +0700

    HienHT/hotfixes/Bottombar

--- lib/src/widgets/custom_bottom_bar.dart
+++ lib/src/widgets/custom_bottom_bar.dart
@@ -64,25 +64,27 @@ final items = [
         // states.unreadNotification.value != 0 ?
         new Positioned(
           right: 0,
-          child: new Container(
-            padding: EdgeInsets.all(1),
-            decoration: new BoxDecoration(
-              color: Colors.red,
-              borderRadius: BorderRadius.circular(6),
-            ),
-            constraints: BoxConstraints(
-              minWidth: 12,
-              minHeight: 12,
-            ),
-            child: new Text(
-              states.unreadNotification.value.toString(),
-              style: new TextStyle(
-                color: Colors.white,
-                fontSize: 8,
+          child: Obx(() {
+            return new Container(
+              padding: EdgeInsets.all(1),
+              decoration: new BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(6),
               ),
-              textAlign: TextAlign.center,
-            ),
-          ),
+              constraints: BoxConstraints(
+                minWidth: 12,
+                minHeight: 12,
+              ),
+              child: new Text(
+                states.unreadNotification.value.toString(),
+                style: new TextStyle(
+                  color: Colors.white,
+                  fontSize: 8,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            );
+          }),
         ), // : SizedBox(),
       ],
     ),
